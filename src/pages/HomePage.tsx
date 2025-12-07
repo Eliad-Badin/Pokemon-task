1104d839cfb2970af9082f8b59ce911d0a362632
Eliad Badin <eliad.badin@moveo.co.il>
2025-12-07 17:23:39 +0200

    Added types and API implementation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,22 +2,61 @@ import Header from "../components/Header/Header";
 import SearchBar from "../components/SearchBar/SearchBar";
 import PokemonCard from "../components/PokemonCard/PokemonCard";
 import "./HomePage.css"
+import { useState, useEffect, useRef } from "react";
+import type { SimplePokemon } from "../types/PokemonTypes";
+import { GetSimplePokemonList } from "../services/api/PokemonApi";
 
 export default function HomePage(){
+    const initialLoad = useRef (false);
+    const [pokemons, setPokemons] = useState<SimplePokemon[]>([]);
+    const [offset, setOffset] = useState<number>(0);
+    const limit = 12;
+    const[isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (!initialLoad.current){
+            initialLoad.current = true;
+            loadMorePokemons();
+        }
+    }, []);
+
+    async function loadMorePokemons() {
+        try {
+            setIsLoading(true);
+            setError(null);
+            const newPokemons = await GetSimplePokemonList(limit, offset);
+            setPokemons(prev => [...prev, ...newPokemons]);
+            setOffset(prev => prev + limit);
+        } catch (err) {
+            setError("Failed to load Pokemon. Please try again.");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
         <div className="home-container">
             <Header />
             <div className="home-content">
                 <SearchBar />
 
-                <div className="pokemon-grid">
-                    <PokemonCard />
-                    <PokemonCard />
-                    <PokemonCard />
-                </div>
 
-                <button className="load-more-btn">Load more...</button>
+            <div className="pokemon-grid">
+                {pokemons.map(p => (
+                <div key={p.id} className="pokemon-card">
+                    <p>#{String(p.id).padStart(3, "0")}</p>
+                    <img src={p.image} alt={p.name} />
+                    <h3>{p.name}</h3>
+                </div>
+                ))}
+                </div>
+            <div className="load-more-wrapper">
+                <button onClick={loadMorePokemons} disabled={isLoading}>
+                {isLoading ? "Loading..." : "Load more..."}
+                </button>
             </div>
+        </div>
         </div>
     );
 }
---
+++ src/services/api/PokemonApi.ts
@@ -0,0 +1,95 @@
+import type {
+     PokemonListResponse, 
+    PokemonDetailsResponse,
+    PokemonSpeciesResponse,
+    SimplePokemon,
+    PokemonFullInfo,
+    PokemonStat
+    } from "../../types/PokemonTypes";
+
+import { BASE_URL } from "../../utils/Strings";
+
+export async function getPokemonList(limit: number, offset: number) : Promise<PokemonListResponse>{ 
+    const response = await fetch (`${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`);
+    if (!response.ok)
+        throw new Error("Failed to fetch Pokemon list!");
+
+    return response.json();
+}
+
+export async function getPokemonDetails(idOrName: string | number): Promise<PokemonDetailsResponse>{
+    const response = await fetch(`${BASE_URL}/pokemon/${idOrName}`);
+    if (!response.ok)
+        throw new Error("Failed to fetch Pokemon details!");
+
+    return response.json();
+}
+
+export async function getPokemonSpeicies (idOrName: string | number): Promise<PokemonSpeciesResponse>{
+    const response = await fetch (`${BASE_URL}/pokemon-species/${idOrName}`);
+    if (!response.ok)
+        throw new Error("Failed to fetch Pokemon spiecies info");
+
+    return response.json();
+}
+
+function mapToSimplePokemon(details: PokemonDetailsResponse): SimplePokemon {
+    const image = 
+    details.sprites.other?.["official-artwork"]?.front_default ?? 
+    details.sprites.front_default ?? "";
+
+    return {
+        id: details.id,
+        name: details.name,
+        image,
+        types: details.types.map (t => t.type.name),
+    };
+}
+
+function mapToPokemonFullInfo(
+    details: PokemonDetailsResponse,
+    species: PokemonSpeciesResponse
+): PokemonFullInfo {
+    const englishEntry = species.flavor_text_entries.find(
+        entry => entry.language.name ==="en");
+    
+    const description = englishEntry ? 
+    englishEntry.flavor_text.replace(/\s+/g, " ") : "No description available.";
+
+    const stats: PokemonStat[] = details.stats.map (s => ({
+        name: s.stat.name,
+        value: s.base_stat,
+    }));
+
+    const totalStats = stats.reduce((sum, s) => sum + s.value, 0);
+
+    const image = 
+    details.sprites.other?.["official-artwork"]?.front_default ?? 
+    details.sprites.front_default ?? "";
+
+    return {
+        id: details.id,
+        name: details.name,
+        image,
+        types: details.types.map(t => t.type.name),
+        description,
+        stats,
+        totalStats,
+    };
+}
+
+export async function GetSimplePokemonList(limit: number, offset: number):
+Promise<SimplePokemon[]> {
+    const list = await getPokemonList(limit, offset);
+    const promises = list.results.map(item => getPokemonDetails(item.name));
+    const detailsArray = await Promise.all(promises); 
+    return detailsArray.map(details => mapToSimplePokemon(details));
+}
+
+export async function getFullPokemonInfo(idOrName: string | number ): 
+Promise<PokemonFullInfo> {
+    const details = await getPokemonDetails(idOrName);
+    const species = await getPokemonSpeicies(idOrName);
+
+    return mapToPokemonFullInfo(details, species);
+}
