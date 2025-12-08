cccf1c680494f09956a294466cf012a12c645dd4
Eliad Badin <eliad.badin@moveo.co.il>
2025-12-08 14:22:04 +0200

    Full detailed information flow - DetailesCard and DetailsPage added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import PokemonDetailsPage from "./pages/PokemonDetailsPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      {/* More pages later */}
+      <Route path="/pokemon/:id" element={<PokemonDetailsPage />} />
     </Routes>
   );
 }
---
+++ src/pages/PokemonDetailsPage.tsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { getPokemonDetails, getPokemonSpeicies } from "../services/api/PokemonApi";
+import type { PokemonDetailsResponse, PokemonSpeciesResponse, PokemonFullInfo } from "../types/PokemonTypes";
+import PokemonDetailsCard from "../components/PokemonDetailsCard/PokemonDetailsCard";
+
+export default function PokemonDetailsPage () {
+    const { id } = useParams();
+
+    const [details, setDetails] = useState<PokemonDetailsResponse | null>(null);
+    const [species, setSpecies] = useState<PokemonSpeciesResponse | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [pokemon, setPokemon] = useState<PokemonFullInfo | null>(null);
+
+    useEffect(() => {
+        async function loadPokemon() {
+            try {
+                setIsLoading(true);
+                setError(null);
+
+                if (!id) return;
+
+                const detailsData = await getPokemonDetails(id);
+                const speciesData = await getPokemonSpeicies(id);
+
+                setDetails(detailsData);
+                setSpecies(speciesData);
+
+                const description =
+                    speciesData.flavor_text_entries.find(
+                        (entry) => entry.language.name === "en"
+                    )?.flavor_text.replace(/\n|\f/g, " ") ??
+                    "No description available.";
+
+                const totalStats = detailsData.stats.reduce(
+                    (sum, s) => sum + s.base_stat,
+                    0
+                );
+
+                const fullInfo: PokemonFullInfo = {
+                    id: detailsData.id,
+                    name: detailsData.name,
+                    image: detailsData.sprites.front_default ?? "",
+                    types: detailsData.types.map((t) => t.type.name),
+                    description,
+                    stats: detailsData.stats.map((s) => ({
+                        name: s.stat.name,
+                        value: s.base_stat,
+                    })),
+                    totalStats,
+                };
+
+                setPokemon(fullInfo);
+
+            } catch (err) {
+                setError("Failed to load pokemon data");
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        loadPokemon();
+    }, [id]);
+
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
+    if (!pokemon) return null; 
+
+    function toggleFavorite() {
+        setIsFavorite((prev) => !prev);
+    }
+
+    return (
+        <div>
+            <Link to="/" className="back-link">
+                ← Home page
+            </Link>
+
+            <PokemonDetailsCard
+                pokemon={pokemon}
+                isFavorite={isFavorite}
+                onToggleFavorite={toggleFavorite}
+            />
+        </div>
+    );
+}
